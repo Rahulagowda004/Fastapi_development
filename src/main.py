8f57355d5a8da1ce35a51946c47cae6140e93a8c
rahulagowda004 <rahulgowda277@gmail.com>
2025-11-05 11:22:04 +0530

    done with individual patients

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Path
 from .utils.utils import get_data
 
 app = FastAPI()
@@ -15,3 +15,10 @@ async def about():
 async def view_patients():
     data = get_data()
     return {"patients": data}
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str = Path(..., description='ID of the patient in the DB', example='P001')):
+    data = get_data()
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404, detail='Patient not found')
